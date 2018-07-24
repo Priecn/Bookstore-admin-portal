472a2b72cb8a956d08f96f9ac918162f589b6883
Priecn <prince.silverarrow@gmail.com>
2018-07-24 19:20:54 +0530

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyCustomMaterialModule } from './custom.material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyCustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
